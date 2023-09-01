48abdfc847a54321a57228cac865da9c2c04f5e3
Uriel Haile <perenganito@gmail.com>
2023-09-01 13:54:49 -0600

    Parallax spring

--- src/moduleParallax/moduleParallax.js
+++ src/moduleParallax/moduleParallax.js
@@ -1,5 +1,6 @@
-import React from "react";
-import {Parallax} from"react-parallax";
+import React, {useRef} from "react";
+//import {Parallax, ParallaxLayer} from"react-parallax";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import image1 from  "../img/1.png";
 import image2 from  "../img/2.png";
 import image3 from  "../img/3.png";
@@ -19,65 +20,35 @@ const insideStyle ={
     transform:"traslate(-50%, -50%)"
 }
 
+export const TextToDisplay = () => {
+      return (<>Despues de una extensa investigacion, el equipo desveló que, al menos en el estado no existen 
+      una base de datos accesible sobra las cifras del suicidio en estta region del pais.Si bien los datos
+      estan abiertamente proporcionados por la secretaria de salud pública y no son capaces de entenderse 
+      por si solos.</>) 
+
+}
+
 export  const ModuleParallax = () => {
+  const ref = useRef();
 return(
     <div>
-       <Parallax bgImage={image1} strength ={200}>
-        <div style={{ height: "100vh"}}>
-        </div>
-       </Parallax>
-       <Parallax >
-        <div >
-            <h2 className="parrafo1" >
-                hace un par de meses, tal vez ya un año, la noticia del suicidio de una estudiante 
-                recorrio los pasillos de mi universidad. Como con la mayoría de sucesos, unos se enteran 
-                por medio de rumores, la información y los datos se transfiguran. Y nosotros como estudiantes,
-                nos enteramos por medio de la directora, de manera oficial, pasadas unas semanas. El ambiente en 
-                los corredores unversitarios se tornó silencioso, gris y sobrio. Realmente no sabias si se debia 
-                discutir, platicar, comentar o debatir sobre el tema. y no de la victima en si o del suceso, si no 
-                del acto de suicidarse. Algunas inquietudes se sembraron en la mente de mis compañeros:¿ en que 
-                situación habrá estado la compañera para tomar tal decision? ¿La universidad puede aportar algo positivo
-                para la prevención de este tipo de sircunstancias? ¿y que hay del gobrierno? ¿cuantos estudiantes tienen
-                este tipo de pensamiento? ¿la mayoria estan en este ranfo de edad universitario? 
-
-                Despues de una extensa investigacion, el equipo desveló que, al menos en el estado no existen 
-                una base de datos accesible sobra las cifras del suicidio en estta region del pais.Si bien los datos
-                estan abiertamente proporcionados por la secretaria de salud pública y no son capaces de entenderse 
-                por si solos. 
-            </h2>
-        </div>
-       </Parallax>
-       <Parallax >
-       <div >
-            <h2 className="parrafo1" >
-                "Pero ¿ que son cien millones de muertos? cuando se ha hecho la guerra apenas se sabe
-                ya nadie lo que es un muerto. y además un hombre muerto solamente tiene peso cuando le han visto 
-                muerto uno; Cien millones de cadaveres no son mas que humo en la imaginación"
-                                                                       -Albert Camus- 
-                                                                   
-            </h2>
-        </div>
-       </Parallax>
-       <Parallax bgImage={image4} strength ={200}>
-        <div style={{ height: "100vh"}}>
-        </div>
-       </Parallax>
-       <Parallax bgImage={image5} strength ={200}>
-        <div style={{ height: "100vh"}}>
-        </div>
-       </Parallax>
-       <Parallax bgImage={image6} strength ={200}>
-        <div style={{ height: "100vh"}}>
-        </div>
-       </Parallax>
-       <Parallax bgImage={image7} strength ={200}>
-        <div style={{ height: "100vh"}}>
-        </div>
-       </Parallax>
-       <Parallax bgImage={image8} strength ={200}>
-        <div style={{ height: "100vh"}}>
-        </div>
-       </Parallax>
+      <Parallax pages={4}  ref={ref}>
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          sticky={{ start: 0.2, end: 2.5 }}
+          style={{ backgroundColor: '#805E73' }}
+        >
+          <h2>Primer texto</h2>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={0.3}
+          speed={0.05}
+          sticky={{ start: 0.3, end: 2.5 }}
+        >
+          <h2>Segundo texto</h2>
+        </ParallaxLayer>
+    </Parallax>
     </div>   
 )
 }
