c558e42f63f243f5685458e8a36ab8ec315587c2
Uriel Haile <perenganito@gmail.com>
2023-09-27 15:49:26 -0600

    Primera versión hecha por Mónica

---
+++ src/mapa.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Map, TileLayer } from "react-leaflet";
+import { useRef } from 'react';
+import osm from './components/osm-providers.js'
+
+import "leaflet/dist/leaflet.css";
+
+const Mapa = () => {
+    // eslint-disable-next-line no-unused-vars
+    const [center, setCenter] = useState({ lat: 13.084622, lng: 80.248357 });
+    const ZOOM_lEVEL = 9;
+    const mapRef = useRef()
+
+    return (
+        <>
+            <div id='map'>
+                <Map center={center} zoom={ZOOM_lEVEL} ref={mapRef}>
+                    <TileLayer
+                        url={osm.maptiler.url} attribution={osm.maptiler.attribution}
+                    />
+                </Map>
+            </div>
+        </>
+    )
+}
+
+export default Mapa
