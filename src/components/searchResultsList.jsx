2259cd930216e7bf2a50d3ea008e6a3ac54fc61f
Adrian <kerpador@gmail.com>
2024-04-26 08:04:50 -0600

    Update searchResultsList.jsx
    Reemplazar la clase para eliminar la pantalla visible

--- src/components/searchResultsList.jsx
+++ src/components/searchResultsList.jsx
@@ -1,16 +1,25 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+import { node } from "prop-types";
 import React from "react";
 
 const SearchResult = ({ result, onResultClick }) => (
-  <div className="result" onClick={() => onResultClick(result)}>
+  <div className="result" id="resultlist"onClick={() => { 
+    onResultClick(result)
+    //aqui debo de modificar
+    const r=document.getElementById('resultlist')
+    r.parentNode.classList.replace('results-list', 'result-hide')
+    } } >
     {result.name}
   </div>
 );
+
 export const SearchResultsList = ({ results, onResultClick }) => (
-  <div className="results-list">
+
+  <div className="results-list" >
     {results.map((result) => (
       <SearchResult key={result.mun_code} result={result} onResultClick={onResultClick} />
-    ))}
+    ))
+    }
   </div>
 );
