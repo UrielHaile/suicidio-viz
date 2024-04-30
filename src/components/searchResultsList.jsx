b20b976310a8ce440106cfc6f89e43b9e24a588d
Adrian <kerpador@gmail.com>
2024-04-30 04:46:51 -0600

    Update searchResultsList.jsx

--- src/components/searchResultsList.jsx
+++ src/components/searchResultsList.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+
 import React from "react";
 
 const SearchResult = ({ result, onResultClick }) => (
   <div className="result" id="resultlist"onClick={() => { 
     onResultClick(result)
-    //aqui debo de modificar
     const r=document.getElementById('resultlist')
     r.parentNode.classList.replace('results-list', 'result-hide')
     } } >
@@ -14,6 +14,7 @@ const SearchResult = ({ result, onResultClick }) => (
 );
 
 export const SearchResultsList = ({ results, onResultClick }) => (
+
   <div className="results-list" >
     {results.map((result) => (
       <SearchResult key={result.mun_code} result={result} onResultClick={onResultClick} />
